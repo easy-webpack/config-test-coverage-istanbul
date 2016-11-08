a89ebd8329db4dcda534ad8210eb107d0ffa258c
Bazyli Brzóska <bazyli.brzoska@gmail.com>
2016-11-08 20:39:51 +0100

    feat(index): allow custom options

--- src/index.ts
+++ src/index.ts
@@ -8,16 +8,14 @@ import * as path from 'path'
  * See: https://github.com/deepsweet/istanbul-instrumenter-loader
  */
 // NOTE: Currently breaks with Webpack >=2
-export = function istanbul({include = undefined, exclude = undefined}:{include?, exclude?} = {}) {
+export = function istanbul({include = undefined, exclude = undefined, options = { 'force-sourcemap': true }}:{include?, exclude?, options?} = {}) {
   return function istanbul(this: WebpackConfigWithMetadata): WebpackConfigWithMetadata {
     return {
       module: {
         rules: get(this, 'module.rules', []).concat([{
           test: /\.(js|ts)$/,
           loader: 'sourcemap-istanbul-instrumenter-loader',
-          query: {
-            'force-sourcemap': true
-          },
+          query: options,
           enforce: 'post',
           include: include || this.metadata.src,
           exclude: exclude || (this.metadata.root ? [path.join(this.metadata.root, 'node_modules')] : []),
